2e613a9293ddfb7c705c1f237d9f1363a96d258d
Marko Oksanen <me@markooksanen.com>
2023-08-16 16:17:15 +0300

    Create curator_feed.js
    juicer -> curator

---
+++ static/js/curator_feed.js
@@ -0,0 +1,11 @@
+<!-- Place <div> tag where you want the feed to appear -->
+<!-- The Javascript can be moved to the end of the html page before the </body> tag -->
+
+  <script type="text/javascript">
+/* curator-feed-default-feed-layout */
+(function(){
+var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
+i.src="https://cdn.curator.io/published/39d4810b-2008-4045-92fa-f8b9230a7e2a.js";
+e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+})();
+</script>
