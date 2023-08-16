8742185771ffc0dca583e8c55fed8434943edd6a
Marko Oksanen <me@markooksanen.com>
2023-08-16 16:31:50 +0300

    Update curator_feed.js

--- static/js/curator_feed.js
+++ static/js/curator_feed.js
@@ -1,8 +1,6 @@
-<script type="text/javascript">
 /* curator-feed-default-feed-layout */
 (function(){
 var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
 i.src="https://cdn.curator.io/published/39d4810b-2008-4045-92fa-f8b9230a7e2a.js";
 e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
 })();
-</script>
